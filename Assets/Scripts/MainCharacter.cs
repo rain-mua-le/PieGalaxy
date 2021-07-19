2283c3c0b93d46d3bcfccdde2ede82fc85b323f4
Rain Le <rainmuale45@gmail.com>
2021-07-19 14:02:17 -0500

    Added character movement

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public const float speed = 3.0f;
+    public int level = 1;
+    public int health = 60;
+    public int attack = 5;
+    public int defense = 5;
+    public int mana = 30;
+    public float crit;
+    private Rigidbody2D rgb;
+
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 pos = rgb.position;
+        pos.x += speed * horizontal * Time.deltaTime;
+        pos.y += speed * vertical * Time.deltaTime;
+        rgb.MovePosition(pos);
+    }
+}
